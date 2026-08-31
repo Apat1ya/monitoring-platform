039f07584b581a56321bc6aa27634b38cb55322b
Viktoriia Dragneva <viktoriiadragneva@gmail.com>
2026-08-31 05:21:22 -0700

    configure JWT authentication in monitor-service; add extract current user ID from JWT subject

--- monitor-service/src/main/java/io/github/apat1ya/monitor/service/support/CurrentUserProvider.java
+++ monitor-service/src/main/java/io/github/apat1ya/monitor/service/support/CurrentUserProvider.java
@@ -1,10 +1,21 @@
 package io.github.apat1ya.monitor.service.support;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 public class CurrentUserProvider {
     public Long getCurrentUserId() {
-        return 1L; //TODO с монитора кафка будет в этот сервис отправлять и тут consumer будет сюда передавать
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(authentication instanceof JwtAuthenticationToken jwtAuthenticationToken)) {
+            throw new IllegalStateException("JWT authentication not found");
+        }
+
+        return Long.valueOf(Objects.requireNonNull(jwtAuthenticationToken.getToken().getSubject()));
     }
 }
